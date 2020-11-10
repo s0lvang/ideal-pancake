adb1041788ea40b5a7f17d783d788bd238c8ad6b
Aslak Hollund <aslakhol@gmail.com>
2020-11-10 18:16:28 +0100

    move model related methods to models

--- trainer/model.py
+++ trainer/model.py
@@ -1,17 +1,23 @@
+import logging
+import os
+
 import numpy as np
+
 from sklearn import compose
 from sklearn import ensemble
 from sklearn import impute
 from sklearn import pipeline
 from sklearn import preprocessing
+from sklearn import model_selection
+from sklearn.metrics import classification_report
 
 from trainer import metadata
 from trainer import utils
+
 from sktime.classification.compose import ColumnEnsembleClassifier
 from sktime.classification.compose import TimeSeriesForestClassifier
 from sktime.transformers.series_as_features.compose import ColumnConcatenator
 from sktime.classification.dictionary_based import BOSSEnsemble
-
 from sktime.transformers.series_as_features.interpolate import TSInterpolator
 
 
@@ -29,3 +35,25 @@ def build_pipeline(flags):
             ("classifier", classifier),
         ]
     )
+
+
+# This method handles all evaluation of the model. Since we don't actually need the prediction for anything it is also handled in here.
+def evaluate_model(model, x_test, y_test):
+    prediction = model.predict(x_test)
+    print(classification_report(y_test, prediction))
+    # Note: for now, use `cross_val_score` defaults (i.e. 3-fold)
+    scores = model_selection.cross_val_score(model, x_test, y_test, cv=2)
+    logging.info(scores)
+
+    return scores
+
+
+# Write model and eval metrics to `output_dir`
+def store_model_and_metrics(model, metrics, output_dir):
+    model_output_path = os.path.join(output_dir, "model", metadata.MODEL_FILE_NAME)
+    metric_output_path = os.path.join(
+        output_dir, "experiment", metadata.METRIC_FILE_NAME
+    )
+
+    utils.dump_object(model, model_output_path)
+    utils.dump_object(metrics, metric_output_path)
--- trainer/task.py
+++ trainer/task.py
@@ -23,7 +23,6 @@ import sys
 import hypertune
 import numpy as np
 from sklearn import model_selection
-from sklearn.metrics import classification_report
 
 from trainer import metadata
 from trainer import model
@@ -39,8 +38,8 @@ def run_experiment(flags):
     )
     pipeline = model.build_pipeline(flags)
     pipeline.fit(x_train, y_train)
-    scores = evaluate_model(pipeline, x_test, y_test)
-    store_model_and_metrics(pipeline, scores, flags.job_dir)
+    scores = model.evaluate_model(pipeline, x_test, y_test)
+    model.store_model_and_metrics(pipeline, scores, flags.job_dir)
 
     # Tuning hyperparameters, currently unused
     # hypertune(scores)
@@ -49,28 +48,6 @@ def run_experiment(flags):
 def get_data_from_feature_selection(dataset):
     columns_to_use = metadata.FEATURE_COLUMNS
     return dataset[columns_to_use]
-
-
-# This method handles all evaluation of the model. Since we don't actually need the prediction for anything it is also handled in here.
-def evaluate_model(model, x_test, y_test):
-    prediction = model.predict(x_test)
-    print(classification_report(y_test, prediction))
-    # Note: for now, use `cross_val_score` defaults (i.e. 3-fold)
-    scores = model_selection.cross_val_score(model, x_test, y_test, cv=2)
-    logging.info(scores)
-
-    return scores
-
-
-# Write model and eval metrics to `output_dir`
-def store_model_and_metrics(model, metrics, output_dir):
-    model_output_path = os.path.join(output_dir, "model", metadata.MODEL_FILE_NAME)
-    metric_output_path = os.path.join(
-        output_dir, "experiment", metadata.METRIC_FILE_NAME
-    )
-
-    utils.dump_object(model, model_output_path)
-    utils.dump_object(metrics, metric_output_path)
 
 
 def hypertune(metrics):
