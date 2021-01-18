3bb136850799b3d8ad9488516de58db96b7b9e3b
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-01-18 15:31:56 +0100

    adds header and trial-number

--- misc/fix_emip_trials.py
+++ misc/fix_emip_trials.py
@@ -6,13 +6,14 @@ import numpy as np
 
 def get_header(file):
     headiter = takewhile(lambda s: s.startswith("##"), file)
-    length = len(list(headiter))
+    headiterlist = [*headiter]
+    length = len(headiterlist)
     headerList = list(map(lambda x: x.strip("##").strip().split(":"), headiter))
     header = dict(filter(lambda x: len(x) == 2, headerList))
     split_on_tab = lambda x: x.split("\t")[1:]
     header = {k: split_on_tab(v) for k, v in header.items()}
     file.seek(0, 0)
-    return header, length
+    return headiterlist, length
 
 
 def remove_nonimportant_messages(df):
@@ -22,12 +23,10 @@ def remove_nonimportant_messages(df):
 
 def mark_trials_and_calibration(df):
     messages = df[df["Type"] == "MSG"]
-    # df = df[: (df["L Raw X [px]"] == messages.iloc[0, 4]).idxmax()]
-
-    # Remove everything before the first message since it is uncalibrated noise
 
     df["status"] = ""
-    df["trial_number"] = 1
+    df["trial_number"] = 0
+    trial_number = 0
     for i in range(len(messages.index) - 1):
         current_message = messages.iloc[i]
         next_message = messages.iloc[i + 1]
@@ -39,6 +38,7 @@ def mark_trials_and_calibration(df):
             == "# Message: instruction_comprehension.jpg"
         ):
             status = "INSTRUCTING"
+            trial_number += 1
         elif "mupliple" in current_message["L Raw X [px]"]:
             status = "TEST"
         else:
@@ -48,11 +48,15 @@ def mark_trials_and_calibration(df):
         next_message_time = next_message.loc["Time"]
         df.loc[
             df["Time"].between(current_message_time, next_message_time),
-            "status",
-        ] = status
+            ["status", "trial_number"],
+        ] = (status, trial_number)
     df.loc[
-        df["Time"].between(next_message_time, df["Time"].iloc[-1]), "status"
-    ] = status  # Get last value aswell
+        df["Time"].between(next_message_time, df["Time"].iloc[-1]),
+        ["status", "trial_number"],
+    ] = (
+        status,
+        trial_number,
+    )  # Get last value aswell
     return df
 
 
@@ -71,5 +75,10 @@ for file in os.listdir(basepath):
         df = pd.read_csv(f, sep="\t", skiprows=length)
         rewritten_df = rewrite_df(df)
         rewritten_df.to_csv(
-            f"../datasets/emip-rewritten/data/{file}"
+            f"../datasets/emip-rewritten/data/{file}", sep="\t"
         )  # remember to prepend the headers after write
+    with open(f"../datasets/emip-rewritten/data/{file}", "r+") as fr:
+        content = fr.read()
+        print(header)
+        fr.seek(0, 0)
+        fr.write("".join(header) + content)
