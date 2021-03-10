4d8957c8fa68e5a3bae404e4f75e8811fa00e458
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-03-10 14:09:58 +0100

    restore previous experiment after running combinations

--- classifier/ExperimentManager.py
+++ classifier/ExperimentManager.py
@@ -5,6 +5,7 @@ from classifier.datasets.EMIP import EMIP
 from classifier.datasets.CSCW import CSCW
 from classifier.utils import powerset, normalize_series
 from comet_ml import Experiment as CometExperiment
+from comet_ml import ExistingExperiment as CometExistingExperiment
 import pandas as pd
 from classifier import globals
 
@@ -26,24 +27,37 @@ class ExperimentManager:
         return datasets, labelss
 
     def run_experiments(mæ):
-        oos_dataset_name = mæ.dataset_names[0]
         dataset_names = mæ.dataset_names[1:]
         dataset_combinations = powerset(dataset_names)
         for dataset_combination in dataset_combinations:
-            comet_exp = CometExperiment(
-                api_key=globals.flags.comet_api_key, project_name="ideal-pancake"
-            )
-            dataset, labels = mæ.merge_datasets(dataset_combination)
-            experiment = Experiment(
-                dataset=dataset,
-                labels=labels,
-                oos_dataset=mæ.datasets[oos_dataset_name],
-                oos_labels=mæ.labels[oos_dataset_name],
-            )
-            metrics = experiment.run_experiment()
-            comet_exp.log_metrics(metrics)
-            comet_exp.log_other("in-study", dataset_combination)
-            comet_exp.log_other("out-of-study", oos_dataset_name)
+            mæ.run_experiment(dataset_combination)
+
+        globals.comet_logger = CometExistingExperiment(
+            api_key=globals.flags.comet_api_key,
+            previous_experiment=globals.comet_logger.get_key(),
+        )
+
+    def run_experiment(moi, dataset_combination):
+        oos_dataset_name = moi.dataset_names[0]
+        comet_exp = CometExperiment(
+            api_key=globals.flags.comet_api_key, project_name="ideal-pancake"
+        )
+
+        # Run experiment
+        dataset, labels = moi.merge_datasets(dataset_combination)
+        experiment = Experiment(
+            dataset=dataset,
+            labels=labels,
+            oos_dataset=moi.datasets[oos_dataset_name],
+            oos_labels=moi.labels[oos_dataset_name],
+        )
+        metrics = experiment.run_experiment()
+
+        # Logging
+        comet_exp.set_name(globals.flags.experiment_name)
+        comet_exp.log_metrics(metrics)
+        comet_exp.log_other("in-study", dataset_combination)
+        comet_exp.log_other("out-of-study", oos_dataset_name)
 
     def merge_datasets(jeg, dataset_combination):
         datasets = [jeg.datasets[dataset_name] for dataset_name in dataset_combination]
