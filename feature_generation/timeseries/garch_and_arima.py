966f535845539a973526634c51d554856c1e16b7
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-03-17 15:06:33 +0100

    switch from arima to arma

--- feature_generation/timeseries/garch_and_arima.py
+++ feature_generation/timeseries/garch_and_arima.py
@@ -7,34 +7,45 @@ import sys
 import os
 
 
-def pdq_combinations(ceiling):
-    p = d = q = range(1, ceiling)
-    return list(itertools.product(p, d, q))
+def poq_combinations(ceiling):
+    p = o = q = range(1, ceiling)
+    return list(itertools.product(p, o, q))
+
+
+def pq_combinations(ceiling):
+    p = q = range(1, ceiling)
+    return list(itertools.product(p, q))
 
 
 # Should take training data, after splitting
-def fit_arima(timeseries, pdq):
-    arima_model = ARIMA(timeseries, order=pdq)
+def fit_arima(timeseries, pq):
+    p, q = pq
+    order = [p, 1, q]
+    print(order, "order")
+    print(len(timeseries))
+    arima_model = ARIMA(timeseries, order)
     fit = arima_model.fit()
     aic = fit.aic
+    print(aic)
     return fit, aic
 
 
 def fit_garch(timeseries, poq):
+    print(poq, "poq")
     garch = arch_model(timeseries, vol="garch", p=poq[0], o=poq[1], q=poq[2])
     fit = garch.fit()
     aic = fit.aic
     return fit, aic
 
 
-def optimize_model(timeseries, param_ceiling, model):
-    params = pdq_combinations(param_ceiling)
+def optimize_model(timeseries, combinations, model):
     best_fit = None
     best_aic = math.inf
 
-    for parameter_set in params:
+    for parameter_set in combinations:
         try:
             fit, aic = model(timeseries, parameter_set)
+            print(aic)
             if aic < best_aic:
                 best_aic = aic
                 best_fit = fit
@@ -46,11 +57,13 @@ def optimize_model(timeseries, param_ceiling, model):
 
 def optimize_garch(timeseries, param_ceiling):
     sys.stdout = open(os.devnull, "w")
-    fit = optimize_model(timeseries, param_ceiling, fit_garch)
+    combinations = poq_combinations(param_ceiling)
+    fit = optimize_model(timeseries, combinations, fit_garch)
     sys.stdout = sys.__stdout__
     return fit.params
 
 
 def optimize_arima(timeseries, param_ceiling):
-    fit = optimize_model(timeseries, param_ceiling, fit_arima)
+    combinations = pq_combinations(param_ceiling)
+    fit = optimize_model(timeseries, combinations, fit_arima)
     return fit.params
