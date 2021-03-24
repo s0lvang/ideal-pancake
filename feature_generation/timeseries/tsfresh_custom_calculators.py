7f840643968e90494110d09129a721ac646abefb
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-03-24 10:15:45 +0100

    deepcopy

--- feature_generation/timeseries/tsfresh_custom_calculators.py
+++ feature_generation/timeseries/tsfresh_custom_calculators.py
@@ -4,44 +4,49 @@ from tsfresh.feature_extraction import feature_calculators
 from .lhipa import calculate_lhipa
 from .markov import calculate_markov
 import numpy as np
+import copy
 
 
 @set_property("fctype", "combiner")
 def arma(d, param):
-    print(d[0], "arma", "beginning")
+    timeseries = copy.deepcopy(d)
+    print(timeseries[0], "arma", "beginning")
     arma_coeff_names = ["exog", "ar", "ma"]
-    best_arma_coeffs = optimize_arma(np.copy(d), param)
-    print(d[0], "arma", "end", best_arma_coeffs)
+    best_arma_coeffs = optimize_arma(timeseries, param)
+    print(timeseries[0], "arma", "end", best_arma_coeffs)
     return [(name, coeff) for name, coeff in zip(arma_coeff_names, best_arma_coeffs)]
 
 
 @set_property("fctype", "combiner")
 def garch(d, param):
-    print(d[0], "garch", "beginning")
+    timeseries = copy.deepcopy(d)
+    print(timeseries[0], "garch", "beginning")
     garch_coeff_names = ["mu", "omega", "alpha", "gamma", "beta"]
-    best_garch_coeffs = optimize_garch(np.copy(d), param)
-    print(d[0], "garch", "end", best_garch_coeffs)
+    best_garch_coeffs = optimize_garch(timeseries, param)
+    print(timeseries[0], "garch", "end", best_garch_coeffs)
     return [(name, coeff) for name, coeff in zip(garch_coeff_names, best_garch_coeffs)]
 
 
 @set_property("fctype", "simple")
 def lhipa(d):
-    print(d[0], "lhipa", "beginning")
+    timeseries = copy.deepcopy(d)
+    print(timeseries[0], "lhipa", "beginning")
     lhipa_value = np.nan
     try:
-        lhipa_value = calculate_lhipa(np.copy(d))
+        lhipa_value = calculate_lhipa(timeseries)
     except Exception as e:
         print(e)
     finally:
-        print(d[0], "lhipa", "end", lhipa_value)
+        print(timeseries[0], "lhipa", "end", lhipa_value)
         return lhipa_value
 
 
 @set_property("fctype", "combiner")
 def markov(d, param):
-    print(d[0], "markov", "beginning")
-    transition_matrix = calculate_markov(np.copy(d), param)
-    print(d[0], "markov", "end")
+    timeseries = copy.deepcopy(d)
+    print(timeseries[0], "markov", "beginning")
+    transition_matrix = calculate_markov(timeseries, param)
+    print(timeseries[0], "markov", "end")
     return [(str(index), value) for index, value in enumerate(transition_matrix)]
 
 
