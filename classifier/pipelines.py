40acda112703b2e86c12116e5253541b6bd669ab
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-03-25 09:59:05 +0100

    adds classification ensemble

--- classifier/pipelines.py
+++ classifier/pipelines.py
@@ -3,10 +3,11 @@ from sklearn import pipeline
 
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel, VarianceThreshold
-from sklearn.linear_model import Lasso, LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.svm import SVR
-from sklearn.ensemble import StackingRegressor
+from sklearn.linear_model import Lasso, LinearRegression, LogisticRegression
+from sklearn.neighbors import KNeighborsRegressor, KNeighborsClassifier
+from sklearn.svm import SVR, SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import StackingRegressor, StackingClassifier
 
 
 def print_and_return(data):
@@ -36,6 +37,27 @@ def build_ensemble_regression_pipeline():
     ]
     final_regressor = LinearRegression()
     regressor = StackingRegressor(estimators=models, final_estimator=final_regressor)
+
+    return pipeline.Pipeline(
+        [
+            # ("printer", FunctionTransformer(print_and_return)),
+            ("zero_variance_filter", VarianceThreshold(threshold=0)),
+            ("Lasso", SelectFromModel(Lasso())),
+            ("classifier", regressor),
+        ]
+    )
+
+
+def build_ensemble_classification_pipeline():
+
+    models = [
+        ("KNN", KNeighborsClassifier()),
+        ("SVM", SVC()),
+        ("RF", RandomForestClassifier()),
+        ("bayes", GaussianNB()),
+    ]
+    final_classifier = LogisticRegression()
+    regressor = StackingClassifier(estimators=models, final_estimator=final_classifier)
 
     return pipeline.Pipeline(
         [
