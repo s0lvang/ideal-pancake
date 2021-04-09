44238c281e3796d67a07ccab4ca4e67f08ec7807
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-04-09 12:21:26 +0200

    remove weights

--- classifier/WeightedAverageEnsemble.py
+++ classifier/WeightedAverageEnsemble.py
@@ -6,8 +6,8 @@ class WeightedAverageEnsemble(VotingRegressor):
     def fit(self, X, y, sample_weight=None):
         X_train, X_test, y_train, y_test = train_test_split(X, y)
         super().fit(X_train, y_train, sample_weight=sample_weight)
-        self.weights = [
-            estimator.score(X_test, y_test) for estimator in self.estimators_
-        ]
+        # self.weights = [
+        #    estimator.score(X_test, y_test) for estimator in self.estimators_
+        # ]
         print(self.weights, "THIS IS THE WEIGTHS")
         return self
