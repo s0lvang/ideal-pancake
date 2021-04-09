a87a696b171db8d34a507b05bd4ed6e9dd1a8c6b
August Sollesnes Solvang <august.s.solvang@gmail.com>
2021-04-09 15:32:32 +0200

    fix ensemble error

--- classifier/WeightedAverageEnsemble.py
+++ classifier/WeightedAverageEnsemble.py
@@ -1,6 +1,8 @@
 from sklearn.ensemble import VotingRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.utils.validation import check_is_fitted
+import numpy as np
 
 
 class WeightedAverageEnsemble(VotingRegressor):
@@ -17,3 +19,23 @@ class WeightedAverageEnsemble(VotingRegressor):
     def score_estimator(self, X, y, estimator):
         prediction = estimator.predict(X)
         return 1 - mean_squared_error(y, prediction, squared=False)
+
+    def predict(self, X):
+        """Predict regression target for X.
+        The predicted regression target of an input sample is computed as the
+        mean predicted regression targets of the estimators in the ensemble.
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape (n_samples, n_features)
+            The input samples.
+        Returns
+        -------
+        y : ndarray of shape (n_samples,)
+            The predicted values.
+        """
+        check_is_fitted(self)
+        print(self._weights_not_none, "weights")
+        print(np.array(self._predict(X)), "pred")
+        return np.average(
+            self._predict(X).astype("float"), axis=1, weights=self._weights_not_none
+        )
